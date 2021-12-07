94614c767b1a80af091739e7706d90685ee3f78d
fe26 <cf9874@gmail.com>
2021-12-08 00:43:28 +0900

    2.4.1 animation (keyframes)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
+const rotation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+transform:rotate(180deg);
+border-radius: 100px;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: tomato;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: powderblue;
+  animation: ${rotation} 2s linear infinite;
+  text-align: center;
+  line-height: 200px;
 `;
 
 function App() {
   return (
     <Father>
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">
-        Log in
-      </Btn>
-      <Input />
-      <Input />
-      <Input />
+      <Box>@ ~ rotation ~ @</Box>
     </Father>
   );
 }
