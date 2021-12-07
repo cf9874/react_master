5d36a60d099c1c88b18472a0dfe35293572f3a1a
fe26 <cf9874@gmail.com>
2021-12-08 02:31:33 +0900

    2.5 nesting + selector

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-  display: flex;
-`;
-
 const rotation = keyframes`
   0% {
     transform:rotate(0deg);
@@ -12,13 +8,21 @@ const rotation = keyframes`
   50% {
 transform:rotate(180deg);
 border-radius: 100px;
-
   }
   100%{
     transform:rotate(360deg);
     border-radius: 0px;
   }
 `;
+const Father = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Text = styled.div`
+  color: tomato;
+`;
+
 const Box = styled.div`
   display: flex;
   height: 200px;
@@ -27,6 +31,12 @@ const Box = styled.div`
   animation: ${rotation} 2s linear infinite;
   justify-content: center;
   align-items: center;
+  ${Text} {
+    &:hover {
+      color: white;
+      font-size: 30px;
+    }
+  }
   span {
     font-size: 20px;
     &:hover {
@@ -43,6 +53,9 @@ function App() {
     <Father>
       <Box>
         <span>@ ~ rotation ~ @</span>
+      </Box>
+      <Box>
+        <Text>ss</Text>
       </Box>
     </Father>
   );
