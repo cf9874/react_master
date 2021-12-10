2dd064f582da7e62f541aa678421402f7b1b1568
fe26 <cf9874@gmail.com>
2021-12-10 18:23:56 +0900

    3.3 Optional Props default value[  ? / ?? ]

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Example from "./example";
 function App() {
   return (
     <div>
-      <Circle bgColor="tomato"/>
-      <Circle bgColor="teal"/>
+      <Circle bgColor="tomato" borderColor="black" text="i'm here" />
+      <Circle bgColor="teal" borderColor="tomato" text="i'm here" />
       <Example name={"john"} age={30} />
     </div>
   );
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,22 +1,35 @@
 import styled from "styled-components";
 
-interface ContainerProps{
+interface ContainerProps {
     bgColor: string;
+    borderColor: string;
+    anyPro?: boolean; // optional
 }
 
 const Container = styled.div<ContainerProps>`
-    width: 200px;
-    height: 200px;
-    background-color:${props=>props.bgColor};
-    border-radius: 100px;
-`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border: 3px solid ${(props) => props.borderColor};
+  border-radius: 100px;
+`;
 
 interface CircleProps {
-    bgColor : string;
+    bgColor: string;
+    borderColor: string;
+    text?: string;
 }
 
-function Circle(props:CircleProps){
-    return <Container bgColor={props.bgColor}/>
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
+    );
 }
+
+// function Circle(props:CircleProps){
+//     return <Container  bgColor={props.bgColor} borderColor={props.borderColor ?? "white"}></Container>
+// }
 
 export default Circle;
